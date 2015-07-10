5090bb74f63cfde313326625e284721cf35a505f
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2015-07-10 18:34:43 +0300

    Second draft of battles is ready. Fairly working and complete. It's using second thread to move the figures without blocking the whole game.

--- battle.py
+++ battle.py
@@ -1,4 +1,110 @@
+import math
+import functools
+
+def sin(angle):
+    return math.sin(math.radians(angle))
+
+def cos(angle):
+    return math.cos(math.radians(angle))
+
+def union(*args):
+    sets = map(lambda x: set(x), args)
+    return list(functools.reduce(lambda x, y: x | y, sets))
+
 class Battle:
     def __init__(self, player1, player2):
         self.black = player1
         self.white = player2
+
+    @staticmethod
+    def width(fields):
+        return len(fields)
+
+    @staticmethod
+    def height(fields):
+        return len(fields[0])
+
+    @staticmethod
+    def rotate(x, y, angle):
+        newX = x * cos(angle) - y * sin(angle)
+        newY = x * sin(angle) + y * cos(angle)
+        return (round(newX), round(newY))
+
+    def isOutside(self, x, y, fields):
+        if (x < 0 or x >= self.width(fields) or
+            y < 0 or y >= self.height(fields)):
+            return True
+        return False
+
+    def hit(self, collNeighs, fields, target):
+        collisions = []
+        for (x, y) in collNeighs:
+            if not self.isOutside(x, y, fields):
+                if fields[x][y] == target:
+                    collisions.append((x, y))
+        return collisions
+
+    def move(self, fields, fromX, fromY, toX, toY):
+        deltaX = toX - fromX
+        deltaY = toY - fromY
+        print(fields)
+        player = fields[fromX][fromY]
+        fields[fromX][fromY] = 0
+        enemy = 2 if player == 1 else 1
+        x, y = fromX, fromY
+        tired = False
+        path = []
+        allWallsHits = []
+        while True:
+            # collision neighbours
+            collNeighs = [(x + deltaX, y + deltaY)]
+            if deltaX != 0 and deltaY != 0:
+                collNeighs.append((x + deltaX, y))
+                collNeighs.append((x, y + deltaY))
+
+            killedEnemies = self.hit(collNeighs, fields, enemy)
+            for (kx, ky) in killedEnemies:
+                fields[kx][ky] = 0
+            if killedEnemies:
+                tired = True
+
+            friendsHit = self.hit(collNeighs, fields, player)
+            if tired and friendsHit:
+                path.append((x, y, killedEnemies))
+                return path
+
+            wallsHit = [(x, y) for (x, y) in collNeighs if self.isOutside(x, y, fields)]
+            common = [1 for x in wallsHit for y in allWallsHits if x == y]
+            if common:
+                path.append((x, y, killedEnemies, 'dead'))
+                return path
+            allWallsHits += wallsHit
+            if tired and wallsHit:
+                path.append((x, y, killedEnemies, 'dead'))
+                return path
+
+            obstacles = union(killedEnemies, friendsHit, wallsHit)
+            print(obstacles)
+
+            if deltaX == 0 or deltaY == 0:
+                if obstacles:
+                    deltaX, deltaY = self.rotate(deltaX, deltaY, 180)
+            else:
+                if len(obstacles) == 1 and obstacles[0] == (x + deltaX, y + deltaY):
+                    deltaX, deltaY = self.rotate(deltaX, deltaY, 180)
+                else:
+                    if (x + deltaX, y) in obstacles:
+                        if deltaX * deltaY < 0:
+                            deltaX, deltaY = self.rotate(deltaX, deltaY, -90)
+                        else:
+                            deltaX, deltaY = self.rotate(deltaX, deltaY, 90)
+                    if (x, y + deltaY) in obstacles:
+                        if deltaX * deltaY > 0:
+                            deltaX, deltaY = self.rotate(deltaX, deltaY, -90)
+                        else:
+                            deltaX, deltaY = self.rotate(deltaX, deltaY, 90)
+
+            x += deltaX
+            y += deltaY
+
+            path.append((x, y, killedEnemies))
--- battle_app.py
+++ battle_app.py
@@ -3,16 +3,45 @@ from panda3d.core import NodePath
 from panda3d.core import Texture
 from direct.showbase import DirectObject
 from panda3d.core import CollisionNode
+from panda3d.core import CollisionSphere
 from panda3d.core import CollisionPolygon
-from panda3d.core import CollisionTraverser, CollisionNode
-from panda3d.core import CollisionHandlerQueue, CollisionRay
 from panda3d.core import Point3
 from panda3d.core import Material
 from panda3d.core import VBase4
 
+import time
+import threading
+
 class BattleEventHandler(DirectObject.DirectObject):
     def __init__(self, app):
         self.accept('mouse1', app.battleHandle, ['left_click'])
+
+class PieceMover(threading.Thread):
+    def __init__(self, piece, moves, fields):
+        threading.Thread.__init__(self)
+        self.moves = moves
+        self.piece = piece
+        self.fields = fields
+
+    def run(self):
+        dead = False
+        print(self.moves)
+        for move in self.moves:
+            moveX, moveY, killed = None, None, None
+            if len(move) == 3:
+                moveX, moveY, killed = move
+            else:
+                print('llama')
+                print(move)
+                moveX, moveY, killed, dead = move
+                dead = True
+            nextField = self.fields[moveX][moveY]
+            self.piece.reparentTo(nextField)
+            if dead:
+                self.piece.removeNode()
+            for corpse in killed:
+                self.fields[corpse[0]][corpse[1]].getChildren()[-1].removeNode()
+            time.sleep(0.3)
 
 class BattleApp:
     def __init__(self):
@@ -45,6 +74,10 @@ class BattleApp:
             self.battleTurn = 0
             self.battlePlayedTurns = 0
 
+            self.clicked = None
+            self.isMoving = False
+            self.mover = None
+
             self.battleFields = []
             for x in range(0, 8):
                 self.battleFields.append([])
@@ -61,45 +94,109 @@ class BattleApp:
                                           Point3(10, -10, 0))
                     cnodePath = field.attachNewNode(CollisionNode('cnode'))
                     cnodePath.node().addSolid(cs)
-                    cnodePath.show()
                     field.setTag('clickable', 'true')
+                    field.setTag('field', 'true')
+                    field.setTag('x', str(x))
+                    field.setTag('y', str(y))
                     field.reparentTo(self.battleRender)
                     self.battleFields[x].append(field)
 
 
             self.battleReady = True
+        if self.mover and not self.mover.is_alive():
+            self.mover = None
+            self.isMoving = False
 
     def destroyBattle(self):
         self.camera.setPos(0, 0, 10)
-        self.camera.setHpr(0, -70, 0)
+        self.camera.setHpr(0, -70, 0)('models/plane')
         self.battleBoard.destroy()
         # TODO check if this works
         self.gameEventHandler = GameEventHandler(self)
         self.battleEventHandler.ignoreAll()
+        self.clicked = None
+        self.mover = None
         # self.battleFields = None
+        self.isMoving = False
         self.battleReady = False
 
     # def battleCurrentPlayer(self):
     #     if self.battleTurn == 0:
     #         return self.battle.black
     #     return self.battle.white
+    def normalizeBattleFields(self):
+        fields = self.battleFields
+        normalized = []
+        for x in fields:
+            normalized.append([])
+            for y in x:
+                figures = y.getChildren()
+                figure = figures[-1] if figures[-1].hasTag('piece') else None
+                if not figure:
+                    normalized[-1].append(0)
+                    continue
+
+                print(figure)
+                mat = figure.getMaterial()
+                print(mat)
+                print(self.blackMaterial)
+                if mat == self.blackMaterial:
+                    normalized[-1].append(1)
+                else:
+                    normalized[-1].append(2)
+        return normalized
 
     def battleHandle(self, event, *args):
         if event == 'left_click':
-            field = self.highlight()
-            if field != None:
-                if self.battlePhase == 'initial':
+            obj = self.highlight()
+            if obj != None:
+                if self.battlePhase == 'initial' and obj.hasTag('field'):
                     figure = self.loader.loadModel('models/circle')
                     figure.setScale(7)
                     if self.battleTurn == 0:
                         figure.setMaterial(self.blackMaterial)
                     else:
                         figure.setMaterial(self.whiteMaterial)
-                    figure.reparentTo(field)
+                    cs = CollisionSphere(0, 0, 0, 1)
+                    cnodePath = figure.attachNewNode(CollisionNode('cnode'))
+                    cnodePath.node().addSolid(cs)
+                    figure.setTag('clickable', 'true')
+                    figure.setTag('piece', 'true')
+                    figure.reparentTo(obj)
                     if self.battlePlayedTurns == 9:
                         self.battlePhase = 'fight'
                     self.battlePlayedTurns += 1
-                self.battleTurn = self.battleTurn ^ 1
+                    self.battleTurn = self.battleTurn ^ 1
+                elif (self.battlePhase == 'fight' and
+                      obj.hasTag('piece') and
+                      not self.isMoving):
+                    color = self.blackMaterial
+                    if self.battleTurn == 1:
+                        color = self.whiteMaterial
+                    if obj.getMaterial() == color:
+                        self.clicked = obj
+                    else:
+                        self.clicked = None
+                elif self.battlePhase == 'fight' and obj.hasTag('field'):
+                    if self.clicked != None:
+                        fromField = self.clicked.getParent()
+                        fromX = int(fromField.getTag('x'))
+                        fromY = int(fromField.getTag('y'))
+                        toX = int(obj.getTag('x'))
+                        toY = int(obj.getTag('y'))
+                        deltaX = toX - fromX
+                        deltaY = toY - fromY
+                        # if have not clicked on the same field
+                        if deltaX | deltaY == 0:
+                            return
+                        if -1 <= deltaX <= 1 and -1 <= deltaY <= 1:
+                            fields = self.normalizeBattleFields()
+                            moves = self.battle.move(fields, fromX, fromY, toX, toY)
+                            self.mover = PieceMover(self.clicked, moves, self.battleFields)
+                            self.mover.start()
+                            self.isMoving = True
+                        self.battleTurn = self.battleTurn ^ 1
+                    self.clicked = None
 
 
 
